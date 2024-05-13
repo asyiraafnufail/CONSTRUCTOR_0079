8bdd1c00b69949b49704a7543361833acd171821
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-13 15:06:10 +0700

    menuliskan access modifier public untuk class mahasiswa

--- memberStaticFunction.cpp
+++ memberStaticFunction.cpp
@@ -6,4 +6,14 @@ class Mahasiswa
 {
     private:
         static int nim;
+    
+    public:
+        int id;
+        string nama;
+
+        void setID();
+        void printAll();
+
+        static void setNim(int pNim) {nim = pNim;}
+        static int getNim() {return nim;}
 }
