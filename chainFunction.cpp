c4146a526aa9838c076009a12430c31cf14eb3ce
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-13 18:56:11 +0700

    menuliskan class Buku

--- chainFunction.cpp
+++ chainFunction.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class Buku
+{
+    string judul;
+
+    public: 
+        Buku setJudul(string judul)
+        {
+            this -> judul = judul;
+            return *this;
+        }
+
+        string getJudul()
+        {
+            return this -> judul;
+        }
+} bukunya;
