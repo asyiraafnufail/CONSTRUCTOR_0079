dc09269d4e1f034d2eaf930319a6b05a1c31e703
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-13 19:17:16 +0700

    menuliskan int main

--- destructor.cpp
+++ destructor.cpp
@@ -45,3 +45,12 @@ void angka::isiData()
         cin >> arr[i];
     }
 }
+
+int main ()
+{
+    angka belajarcpp(3); //constructor dipanggil
+    angka*ptrBelajarcpp = new angka (5); //constructor dipanggil
+    delete ptrBelajarcpp; //destructor dipanggil
+
+    return 0;
+}
